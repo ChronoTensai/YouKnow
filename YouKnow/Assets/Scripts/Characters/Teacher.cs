0299e2744146cf2ff241c4d03b13be8d2d01bfe3
Elyseus <emilianosimoes@gmail.com>
2018-01-27 20:18:48 -0300

    Movement Teacher
    funciona mejor, pero el tema del voveler al anterior no. pero esta mejor

--- YouKnow/Assets/Scripts/Characters/Teacher.cs
+++ YouKnow/Assets/Scripts/Characters/Teacher.cs
@@ -12,17 +12,21 @@ public class Teacher : Enemy {
 	
 	public Node StartNode;
     public Node[] FollowNode;
-    public int point = 0;
-    public float speed = 10f;
-    private float closeEnouth = 0.5f;
+    private Node ToFollow;
+    private Node BeforeFollow;
+    private bool desicion = false;
+    //private int point = 0;
+    public float speed = 20f;
+    private float closeEnough = 0f;
 
 
 
     void Start () {
 	    Invoke("LookScholars", Random.Range(minTimeToTurn,maxTimeToTurn));
         this.transform.SetPositionAndRotation(StartNode.transform.position,Quaternion.LookRotation(StartNode.transform.position- this.transform.position, transform.TransformDirection(Vector3.forward))); //No se como hacer que mire en el sentido que se va a dirigir
-        
-	}
+        ToFollow = FollowNode[0];
+
+    }
 	
 	void LookScholars()
 	{
@@ -70,16 +74,57 @@ public class Teacher : Enemy {
 
     void Move()
     {
-        Quaternion rotation = Quaternion.LookRotation(FollowNode[point].transform.position - this.transform.position, transform.TransformDirection(Vector3.back));
+        Quaternion rotation = Quaternion.LookRotation(ToFollow.transform.position - this.transform.position, transform.TransformDirection(Vector3.forward));
         transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
 
-        this.transform.position = Vector3.MoveTowards(this.transform.position, FollowNode[point].transform.position, (Time.deltaTime/10) * speed);
-        if (Vector3.Distance(this.transform.position, FollowNode[point].transform.position) < closeEnouth)
+        this.transform.position = Vector3.MoveTowards(this.transform.position, ToFollow.transform.position, (Time.deltaTime/10) * speed);
+        if (Vector3.Distance(this.transform.position, ToFollow.transform.position) <= closeEnough)
         {
-            if (point + 1 < FollowNode.Length)
-                point++;
-            else
-                point = 0;
+          //  if (!BeforeFollow)
+          //      BeforeFollow.LastVisited = false;
+            ToFollow.LastVisited = true;
+          //  BeforeFollow = ToFollow;
+
+            while (!desicion)
+            {
+                switch (Random.Range(1, 5))
+                {
+                    case 1:
+                        if (ToFollow.Top && !ToFollow.Top.LastVisited)
+                        {
+                            ToFollow.LastVisited = false;
+                            ToFollow = ToFollow.Top;
+                            desicion = true;
+                        }
+                        break;
+                    case 2:
+                        if (ToFollow.Right && !ToFollow.Right.LastVisited)
+                        {
+                            ToFollow.LastVisited = false;
+                            ToFollow = ToFollow.Right;
+                            desicion = true;
+                        }
+                        break;
+                    case 3:
+                        if (ToFollow.Bot && !ToFollow.Bot.LastVisited)
+                        {
+                            ToFollow.LastVisited = false;
+                            ToFollow = ToFollow.Bot;
+                            desicion = true;
+                        }
+                        break;
+                    case 4:
+                        if (ToFollow.Left && !ToFollow.Left.LastVisited)
+                        {
+                            ToFollow.LastVisited = false;
+                            ToFollow = ToFollow.Left;
+                            desicion = true;
+                        }
+                        break;
+                }
+                
+            }
+            desicion = false;
 
         }
     }
