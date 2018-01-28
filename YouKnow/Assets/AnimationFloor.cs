7f8defb5874fafbdbc33920456eea002e3599b4a
Tensai <jonathan.bengoa@gmail.com>
2018-01-28 09:27:14 -0300

    Add AnimationFloor.cs Add Warning

---
+++ YouKnow/Assets/AnimationFloor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationFloor : MonoBehaviour {
+
+    // Use this for initialization
+    SpriteRenderer _sprite;
+	void Start () {
+        _sprite = this.gameObject.GetComponent<SpriteRenderer>();
+        InvokeRepeating("AnimateThis", 0.5f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void AnimateThis ()
+    {
+
+        if (DoFlip())
+        { 
+            _sprite.flipX = !_sprite.flipX;
+            if (DoFlip())
+            {
+                _sprite.flipY = !_sprite.flipY;
+            }
+        }
+        else
+        {
+            _sprite.flipY = !_sprite.flipY;
+        }
+            
+
+    }
+
+    bool DoFlip()
+    {
+        int random = Mathf.RoundToInt(Random.Range(0, 2));
+        random = random == 2 ? 1 : random;
+
+        return random == 1;
+    }
+}
---
+++ YouKnow/Assets/BallIndicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallIndicator : MonoBehaviour {
+
+    public GameObject Warning;
+    private Transform Teacher;
+
+    private bool ShowingWarning = false;
+
+    void Start()
+    {
+        Teacher = GameObject.FindGameObjectWithTag("Teacher").transform;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 teacherFront = Teacher.TransformDirection(Vector2.up);
+        Vector2 direction = this.transform.position - Teacher.position;
+        if (Vector2.Dot(teacherFront, direction) > 0)
+        {
+            if (!ShowingWarning)
+            {
+                Warning.SetActive(true);
+                ShowingWarning = true;
+            }
+        }
+        else if (ShowingWarning)
+        {
+            Warning.SetActive(false);
+            ShowingWarning = false;
+        }
+    }
+}
