72c567342be2ec69239e6218527ee26c8dc9a770
Elyseus <emilianosimoes@gmail.com>
2018-01-28 00:06:33 -0300

    pop-ups

---
+++ YouKnow/Assets/Scripts/winPrompt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinPrompt : MonoBehaviour {
+
+
+    private GameObject obj;
+
+    public void Start()
+    {
+        obj = GameObject.FindGameObjectWithTag("winPrompt");
+        obj.SetActive(false);
+    }
+
+
+    public void Update()
+    {
+
+        if (MODEL.GAME_STATE == GameStates.Win) {
+            obj.SetActive(true);
+        }
+    }
+
+
+
+    public void LoadLevel()
+    {
+        Debug.Log(MODEL.GAME_STATE);
+        obj.SetActive(false);
+        SceneManager.LoadScene(SCENE.LEVEL_TWO);
+    }
+
+}
+
